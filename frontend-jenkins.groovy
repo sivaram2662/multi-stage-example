a47ce2ac6b2083f11e8c2f188a41d66ef64d1503
sivaram2662 <106672296+sivaram2662@users.noreply.github.com>
2023-06-02 22:25:21 +0530

    changes

--- frontend-jenkins.groovy
+++ frontend-jenkins.groovy
@@ -8,8 +8,8 @@ pipeline {
                git branch: 'main', url: 'https://github.com/sivaram2662/container-micro-frontned.git'
                 sh "aws ecr get-login-password --region ap-south-1 | sudo docker login --username AWS --password-stdin 315073111691.dkr.ecr.ap-south-1.amazonaws.com"
                sh "sudo docker build -t dumy ."
-               sh "sudo docker tag dumy:latest 315073111691.dkr.ecr.ap-south-1.amazonaws.com/dumy:latest"
-               sh "sudo docker push 315073111691.dkr.ecr.ap-south-1.amazonaws.com/dumy:latest"
+               sh "sudo docker tag dumy:latest 315073111691.dkr.ecr.ap-south-1.amazonaws.com/dumy:$BUILD_NUMBER"
+               sh "sudo docker push 315073111691.dkr.ecr.ap-south-1.amazonaws.com/dumy:$BUILD_NUMBER"
 
             }
 
